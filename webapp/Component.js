2b549feeaa174de05429430f92e67263d824dea6
mariuszwielgus <mariusz.wielgus@snpgroup.com>
2021-02-11 13:24:22 +0100

    Initial commit

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("ZWS_DEMO.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("ZWS_DEMO.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
